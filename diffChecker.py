cd34d4759ce7955958f3dca186bb388208953d8d
zaidiaz32 <raffexrivaille@gmail.com>
2025-06-18 20:39:21 -0600

    code changes

--- diffChecker.py
+++ diffChecker.py
@@ -5,7 +5,6 @@ img2 = Image.open('Second.jpg')
 
 difference = ImageChops.difference(img1, img2)
 if difference.getbbox():
-    difference.show()
     difference.save("difference_output.jpg")
 else:
     print("Images are identical.")
